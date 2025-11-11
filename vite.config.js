22597bb4757af3188f6d1494817422a6dd31ce8f
unknown <dimasdimpi90@gmail.com>
2025-11-11 14:10:53 +0700

    clean update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: '/',
+  base: process.env.NETLIFY ? '/' : '/portofolio-react/',
   plugins: [react()],
   // HAPUS baris 'assetsInclude: ['**/*.glb'],' DARI SINI
   resolve: {
